a75eb6118a8b40b1e1e96e8dda6f8e52c6ad923a
AzizTurki356 <62173783+AzizTurki356@users.noreply.github.com>
2020-05-30 16:16:54 +0100

    Aziz Turki Final Arkanoide

---
+++ New Unity Project (3)/Assets/rackette.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rackette : MonoBehaviour
+{
+// Mouvement Speed  
+public float speed = 150;
+void FixedUpdate () {
+ // Get Horizontal Input   
+float h = Input.GetAxisRaw("Horizontal");
+
+
+// Set Velocity(mouvement direction * speed)
+GetComponent<Rigidbody2D>().velocity = Vector2.right * h * speed * Time.deltaTime;
+}
+}
